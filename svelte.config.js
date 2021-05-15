ffdbeaf420e459670f59a8aa01423bccc8f2eebf
krist7599555 <krist7599555@gmail.com>
2021-05-15 20:06:24 +0700

    📖 DOC: docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess'; 
+import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
+		// host: "https://krist7599555.github.io",
+		adapter: adapter(),
+		paths: {
+			base: "/webgl-realtimecg-project"
+			// https://krist7599555.github.io/webgl-realtimecg-project
+		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
